2c4b8094d74ddca24f61834e9325b24c6156b20e
SoniaSantanaFilha <filhasonia2311@gmail.com>
2024-02-06 15:15:41 -0300

    finalizando as classes de dominio

--- src/main/java/io/github/soniasantana/bookstore/domain/Livro.java
+++ src/main/java/io/github/soniasantana/bookstore/domain/Livro.java
@@ -1,13 +1,19 @@
 package io.github.soniasantana.bookstore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
-
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
-
+    @ManyToMany
+    @JoinColumn(name= "categoria_id")
     private Categoria categoria;
 
     public Livro() {
