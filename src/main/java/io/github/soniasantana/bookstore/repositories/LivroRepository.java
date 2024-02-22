c8e269b386882b21ea07e22e6bd9b82ef0d3f9f6
SoniaSantanaFilha <filhasonia2311@gmail.com>
2024-02-22 19:43:35 -0300

    Correção na classe LivroRepository

--- src/main/java/io/github/soniasantana/bookstore/repositories/LivroRepository.java
+++ src/main/java/io/github/soniasantana/bookstore/repositories/LivroRepository.java
@@ -1,9 +1,10 @@
 package io.github.soniasantana.bookstore.repositories;
 
 import io.github.soniasantana.bookstore.domain.Categoria;
+import io.github.soniasantana.bookstore.domain.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
 }
