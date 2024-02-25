7a36cfe88e2ad8ea7a55e8b1f25aedd126336970
SoniaSantanaFilha <filhasonia2311@gmail.com>
2024-02-24 21:25:07 -0300

    Ajustes finalizados no backend

--- src/main/java/io/github/soniasantana/bookstore/domain/Livro.java
+++ src/main/java/io/github/soniasantana/bookstore/domain/Livro.java
@@ -1,8 +1,10 @@
 package io.github.soniasantana.bookstore.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.Objects;
 @Entity
@@ -11,8 +13,17 @@ public class Livro implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotEmpty(message = "Campo TÍTULO é requerido")
+    @Length(min = 3, max = 50, message = "O campo TÍTULO tem que ter entre 3 e 50 caracteres")
     private String titulo;
+
+    @NotEmpty(message = "Campo NOME DO AUTOR é requerido")
+    @Length(min = 3, max = 50, message = "O campo NOME DO AUTOR tem que ter entre 3 e 50 caracteres")
     private String nome_autor;
+
+    @NotEmpty(message = "Campo TEXTO é requerido")
+    @Length(min = 10, max = 2000000, message = "O campo TEXTO tem que ter entre 10 e 2000000 caracteres")
     private String texto;
 
     @JsonIgnore
--- src/main/java/io/github/soniasantana/bookstore/dtos/LivroDTO.java
+++ src/main/java/io/github/soniasantana/bookstore/dtos/LivroDTO.java
@@ -1,7 +1,9 @@
 package io.github.soniasantana.bookstore.dtos;
 
 import io.github.soniasantana.bookstore.domain.Livro;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class LivroDTO implements Serializable {
@@ -9,6 +11,9 @@ public class LivroDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
+
+    @NotEmpty(message = "Campo TÍTULO é requerido")
+    @Length(min = 3, max = 50, message = "O campo TÍTULO tem que ter entre 3 e 50 caracteres")
     private String titulo;
 
     public LivroDTO() {
@@ -16,6 +21,7 @@ public class LivroDTO implements Serializable {
     }
 
     public LivroDTO(Livro obj) {
+        super();
         this.id = obj.getId();
         this.titulo = obj.getTitulo();
     }
